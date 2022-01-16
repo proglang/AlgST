ff22351cdf5259635392d97eef7c0b4289e7ce91
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2022-01-16 15:05:27 +0100

    [driver] Simplify source specification
    The other did not work as expected

--- src/AlgST/CommandLine.hs
+++ src/AlgST/CommandLine.hs
@@ -21,14 +21,12 @@ import System.FilePath
 
 data Source
   = SourceFile !FilePath
-  | SourceLiteral !String
   | SourceStdin
   deriving (Show)
 
 sourcePrettyName :: Source -> Maybe String
 sourcePrettyName = \case
   SourceFile fp -> Just (normalise fp)
-  SourceLiteral _ -> Nothing
   SourceStdin -> Nothing
 
 sourceIsFile :: Source -> Bool
@@ -45,41 +43,14 @@ readSource :: Source -> IO String
 readSource = \case
   SourceStdin -> getContents
   SourceFile fp -> readFile fp
-  SourceLiteral s -> pure s
 
--- No arguments: STDIN
--- One argument: FILE       (can be forced with --file)
--- 1+ arguments: error
---
--- One argument + --file: FILE
--- 1+ arguments + --src: LITERAL
 sourceParser :: O.Parser Source
-sourceParser = sourceFile <|> sourceLiteral <|> sourceImplicit
+sourceParser =
+  fmap (maybe SourceStdin SourceFile)
+    . O.optional
+    . O.strArgument
+    $ mconcat [O.metavar "FILE", O.help fpHelp]
   where
-    sourceFile =
-      let flagOpts =
-            mconcat
-              [ O.long "file",
-                O.help "Read source from given path."
-              ]
-       in O.flag' SourceFile flagOpts <*> O.strArgument fpOpts
-    sourceLiteral =
-      let flagOpts =
-            mconcat
-              [ O.long "src",
-                O.help
-                  "Interpret arguments as literal AlgST source. Multiple \
-                  \arguments are joined by newlines."
-              ]
-          argOpts = O.metavar "SOURCE"
-       in O.flag' (SourceLiteral . unlines) flagOpts <*> some (O.strArgument argOpts)
-    sourceImplicit =
-      fmap (maybe SourceStdin SourceFile)
-        . O.optional
-        . O.strArgument
-        $ fpOpts <> O.help fpHelp
-    fpOpts =
-      mconcat [O.metavar "FILE"]
     fpHelp =
       "If a FILE is given input will be read from this file. Otherwise it \
       \will be read from STDIN."
