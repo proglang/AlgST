10a31e1a0443d2726417f443713e87e2fed11e8a
Janek Spaderna <janek@spaderna.eu>
2023-01-09 14:33:59 +0100

    Merge example tests with unit tests

--- tests/ExamplesSpec.hs
+++ tests/ExamplesSpec.hs
@@ -1,19 +1,20 @@
-module Main (main) where
+module ExamplesSpec where
 
 import Data.Foldable
 import Data.Functor
+import Data.List qualified as List
 import Paths_AlgST
 import System.Directory
 import System.FilePath
 import System.Process.Typed
-import Test.Hspec
+import Test
 
-main :: IO ()
-main = hspec do
+spec :: Spec
+spec = parallel do
   (examplesDir, allExamples) <- runIO do
     examplesDir <- getDataDir <&> (</> "examples")
     allExamples <- listDirectory examplesDir
-    pure (examplesDir, allExamples)
+    pure (examplesDir, List.sort allExamples)
 
   -- We might want to throw a `parallel` in here but (at least at the moment)
   -- it does not seem as if the runtime would actually decrease.
